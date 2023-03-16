43a49defcf8aa8dbad5476f896e1700d89659467
khsamuelsy <khsamuelsy@gmail.com>
2023-03-17 07:44:30 +0800

    Delete FAA_Cal_SubBout_PR_Mirror.m

--- FreeArenaCode/FhAnalysis/paramcal/localcal/FAA_Cal_SubBout_PR_Mirror.m
+++
@@ -1,5 +0,0 @@
-clear all;clc;close all;
-FAA_Cal_SubBout_1_PR_Mirror; % cal latency,distance,frequency
-FAA_Cal_SubBout_2_PR_Mirror; % cal others
-save('SubBout_PR_Mirror.mat','cal');
-clear all;clc;close all;

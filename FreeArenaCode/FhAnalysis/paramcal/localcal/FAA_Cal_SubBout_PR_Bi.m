992de92a145b0b2a9599bbe7e88873fa9cb30ccf
khsamuelsy <khsamuelsy@gmail.com>
2023-03-17 07:45:31 +0800

    Delete FAA_Cal_SubBout_PR_Bi.m

--- FreeArenaCode/FhAnalysis/paramcal/localcal/FAA_Cal_SubBout_PR_Bi.m
+++
@@ -1,5 +0,0 @@
-clear all;clc;close all;
-FAA_Cal_SubBout_1_PR_Bi; % cal latency,distance,frequency
-FAA_Cal_SubBout_2_PR_Bi; % cal others
-save('SubBout_PR_Bi.mat','cal');
-clear all;clc;close all;
